c18b26662e0fa2f84d35a5020c427f88d932789c
GSamridhi09 <90111640+GSamridhi09@users.noreply.github.com>
2021-09-19 00:30:00 +0530

    Create 2.PrintForward.java

---
+++ Recursion/2.PrintForward.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+    public static void print(int n)
+    {
+       if(n==0)
+       {
+           return ;
+       }
+      
+      print(n-1);
+     System.out.print(n+" ");
+      
+    }
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		print(n);
+
+	}
+}
